7ba687d7ee0e45deb7b5961c3683f3c4fc3582a5
MohammedNihalkp <nihalkp6000gmail.com>
2022-03-16 17:35:15 +0530

    new one

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("New"),
       ),
       body: Center(
         child: Column(
